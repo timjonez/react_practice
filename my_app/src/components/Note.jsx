3d458a749ee453abcec9e8ce5913727901befdca
Tim Jones <timjonez@protonmail.com>
2020-05-13 10:57:48 -0400

    added my_app

---
+++ my_app/src/components/Note.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Note(){
+  return(
+    <div className='note'>
+      <h1 >Title</h1>
+      <p>this is the note </p>
+    </div>
+  )
+}
+
+
+export default Note;
